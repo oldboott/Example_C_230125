fe02268a3dd3505cfe16b39dae5fd31e2cefb132
oldboott <oldboott@gmail.com>
2023-01-28 11:56:22 +0400

    Сделали две задачи: найти квадрат и проверить является ли число квадратом другого

---
+++ Example008/Program.cs
@@ -0,0 +1,7 @@
+﻿// Найти квадрат числа
+
+Console.Clear();
+Console.WriteLine("Введите число: ");
+int number = int.Parse(Console.ReadLine()!);
+int sqr = number * number;
+Console.WriteLine($"Квадрат числа {number} равен {sqr}");
---
+++ Example009/Program.cs
@@ -0,0 +1,20 @@
+﻿// Написать программу которая на вход принимает два числа и проверяет является ли первое число квадратом второго
+// а = 25, б = 5 > да
+
+// Написать программу которая будет выводить название дня недели по его номеру
+
+Console.Clear();
+Console.WriteLine ("Введите число: ");
+int number1 = int.Parse(Console.ReadLine()!);
+Console.WriteLine ("Введите второе число: ");
+int number2 = int.Parse(Console.ReadLine()!);
+
+if (number2 * number2 == number1)
+{
+Console.WriteLine($"{number1} является квадратом {number2}");
+}
+else
+{
+    Console.WriteLine($"{number1} не является квадратом {number2}");
+}
+
