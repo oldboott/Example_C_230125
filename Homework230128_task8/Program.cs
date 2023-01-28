af1065b3abdae15e367968093dcb6ea5fd91c86c
oldboott <oldboott@gmail.com>
2023-01-28 15:32:02 +0400

    HomeWork 230128

---
+++ Homework230128_task8/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// 5 -> 2, 4
+// 8 -> 2, 4, 6, 8
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine()!);
+int number = 1;
+
+while (number <= N)
+{
+    if (number % 2 == 0) 
+    Console.Write($"{number}, ");
+    number++;
+}
---
+++ Homework_task4/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+// 2, 3, 7 -> 7
+// 44 5 78 -> 78
+// 22 3 9 -> 22
+
+Console.Clear();
+
+Console.Write("Введите первое число: ");
+int FirstNum = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите второе число: ");
+int SecondNum = int.Parse(Console.ReadLine()!);
+
+Console.Write("Введите третье число: ");
+int ThirdNum = int.Parse(Console.ReadLine()!);
+
+int max = FirstNum;
+
+if (SecondNum > max) max = SecondNum;
+if (ThirdNum > max) max = ThirdNum;
+
+Console.WriteLine($"Максимальное число: {max}");
+
+
+
